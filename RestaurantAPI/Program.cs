4584e59202c9fa908a2375ae5e01bfbbcb8408fe
Cebix90 <cebulmat@gmail.com>
2023-10-23 16:41:54 +0200

    Added NLog.config and Exception Handling Method.

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using NLog.Web;
 using RestaurantAPI;
 using RestaurantAPI.Entities;
+using RestaurantAPI.Middleware;
 using RestaurantAPI.Services;
 using RestaurantAPI.Services.IServices;
 
@@ -9,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // NLog: Setup NLog for Dependency injection
 builder.Logging.ClearProviders();
-builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+builder.Logging.SetMinimumLevel(LogLevel.Trace);
 builder.Host.UseNLog();
 
 // Add services to the container.
@@ -19,6 +20,7 @@ builder.Services.AddDbContext<RestaurantDbContext>();
 builder.Services.AddScoped<RestaurantSeeder>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -37,6 +39,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
