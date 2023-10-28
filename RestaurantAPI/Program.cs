b904025a497da68fd187a273da6428488ba6f989
Cebix90 <cebulmat@gmail.com>
2023-10-29 01:33:56 +0200

    Added UserContextService and HttpContextAccessor

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -59,6 +59,8 @@ builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddScoped<IRestaurantService, RestaurantService>();
 builder.Services.AddScoped<IDishService, DishService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IUserContextService, UserContextService>();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
